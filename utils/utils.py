4a64d833ea009492ea35fad927c79a2257569146
jonahanton <jonahlouisanton@gmail.com>
2022-02-01 19:47:31 +0000

    Edit to utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,4 @@
 def score(rides, B):
-
     score = 0
     for ride in rides:
         if ride.finished:
@@ -12,6 +11,24 @@ def score(rides, B):
 
 def ride_can_finish(T, ride):
     return T <= ride.latest_time
+
+
+def distance_to_ride(location, ride):
+    x, y = location
+    ride_x, ride_y = ride.start
+    return abs(x - ride_x) + (y - ride_y)
+
+
+def nearest_ride(car, rides):
+    closest_ride = None
+    closest_dist = float("inf")
+    for ride in rides:
+        dist = distance_to_ride(car.location, ride)
+        if dist < closest_dist:
+            closest_ride = ride
+            closest_dist = dist
+    return closest_ride
+
 
 
 if __name__ == "__main__":
